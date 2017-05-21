da9f8144a1a79d420e6e372ed631a5ccd6b57033
N0taN3rd <n0tan3rd@gmail.com>
2017-05-21 13:26:19 -0400

    more flexibility

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-const fs = require('fs-extra')
-const Promise = require('bluebird')
-const bsplit = require('binary-split')
-const CDXJEntry = require('./lib/cdxjEntry')
-
-module.exports = function readCDXJ (path) {
-  return new Promise((resolve, reject) => {
-    let cdxEntries = []
-    fs.createReadStream(path)
-      .pipe(bsplit())
-      .on('data', line => {
-        cdxEntries.push(new CDXJEntry(line))
-      })
-      .on('end', () => {
-        resolve(cdxEntries)
-      })
-  })
-}
+const CDXJReader = require('./lib/cdxjReader')
+module.exports = CDXJReader
